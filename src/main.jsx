4a7b04bbbdcc04b9a70c150f17d081231003d5a9
Emina Toric <137021140+EminaToric@users.noreply.github.com>
2026-03-22 19:01:06 -0400

    Create main.jsx

---
+++ src/main.jsx
@@ -0,0 +1,10 @@
+import React from "react"
+import ReactDOM from "react-dom/client"
+import EarlyLearningDashboard from "./EarlyLearningDashboard"
+import "./index.css"
+
+ReactDOM.createRoot(document.getElementById("root")).render(
+  <React.StrictMode>
+    <EarlyLearningDashboard />
+  </React.StrictMode>
+)
